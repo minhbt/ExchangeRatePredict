4597ea924123c939338b4faf3cded22234e29f8d
Minh Bui <minhbt.dev@gmail.com>
2019-04-24 09:43:32 +0700

    Update CI&CD

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 repoFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $repoFolder
 
-koreBuildZip="https://github.com/aspnet/KoreBuild/archive/dev.zip"
-if [ ! -z $KOREBUILD_ZIP ]; then
-    koreBuildZip=$KOREBUILD_ZIP
+onsolveEPZip="https://github.com/minhbt/ExchangeRatePredict/blob/master/Deliveries/Archive.zip"
+if [ ! -z $ONSOLVEEP_ZIP ]; then
+    onsolveEPZip=$ONSOLVEEP_ZIP
 fi
 
 buildFolder=".build"
 buildFile="$buildFolder/KoreBuild.sh"
 
 if test ! -d $buildFolder; then
-    echo "Downloading KoreBuild from $koreBuildZip"
+    echo "Downloading KoreBuild from $onsolveEPZip"
 
     tempFolder="/tmp/KoreBuild-$(uuidgen)"
     mkdir $tempFolder
@@ -19,9 +19,9 @@ if test ! -d $buildFolder; then
     localZipFile="$tempFolder/korebuild.zip"
 
     retries=6
-    until (wget -O $localZipFile $koreBuildZip 2>/dev/null || curl -o $localZipFile --location $koreBuildZip 2>/dev/null)
+    until (wget -O $localZipFile $onsolveEPZip 2>/dev/null || curl -o $localZipFile --location $onsolveEPZip 2>/dev/null)
     do
-        echo "Failed to download '$koreBuildZip'"
+        echo "Failed to download '$onsolveEPZip'"
         if [ "$retries" -le 0 ]; then
             exit 1
         fi
